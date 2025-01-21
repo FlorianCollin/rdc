cca17d43ca29483b745a4eb004275b905902f691
FlorianCollin <flocol1302@gmail.com>
2025-01-21 14:48:52 +0100

    LED Ticker

--- main.cpp
+++ main.cpp
@@ -4,48 +4,53 @@
  */
 
 #include "mbed.h"
-#include <stdio.h>
 using namespace std::chrono;
 
 // Blinking rate in milliseconds
-#define BLINKING_RATE     500ms
+// #define BLINKING_RATE     500ms
 
 
-InterruptIn button(BUTTON1);
+// InterruptIn button(BUTTON1);
 DigitalOut led(LED1);
-Timer t;
+// Timer t;
+
+Ticker flipper;
+// char flag_to_print;
+// float current_timer_delta;
 
 
+// void handler_rise() {
+//     current_timer_delta = t.read_ms();
+// }
+// 
+// void handler_fall() {
+//     current_timer_delta = t.read_ms() - current_timer_delta;
+//     flag_to_print = 1;
+//     
+// }
 
-char flag_to_print;
-float current_timer_delta;
 
-
-void handler_rise() {
-    current_timer_delta = t.read_ms();
-}
-
-void handler_fall() {
-    current_timer_delta = t.read_ms() - current_timer_delta;
-    flag_to_print = 1;
-    
+void flip() {
+    led = !led;
 }
 
 int main()
 {
-    flag_to_print = 0;
-    t.start();
+    // flag_to_print = 0;
+    // t.start();
 
-    button.enable_irq();
-    button.rise(&handler_rise); 
-    button.fall(&handler_fall); 
-    printf("Hello world\n");
-
-    while (true) {
-        if (flag_to_print) {
-            flag_to_print = 0;
-            printf("timer : %f ms\n", current_timer_delta);
-        }
-        ThisThread::sleep_for(BLINKING_RATE);
+    // button.enable_irq();
+    // button.rise(&handler_rise); 
+    // button.fall(&handler_fall); 
+    // printf("Hello world\n");
+    flipper.attach(&flip, 2.0);
+    while(true) {
     }
+    // while (true) {
+    //     if (flag_to_print) {
+    //         flag_to_print = 0;
+    //         printf("timer : %f ms\n", current_timer_delta);
+    //     }
+    //     ThisThread::sleep_for(BLINKING_RATE);
+    // }
 }
